4345c9f674f90fe08052ce403f5e36fc55140b38
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-01-07 21:07:09 +0700

    Update LoginService.php

--- app/Services/Auth/LoginService.php
+++ app/Services/Auth/LoginService.php
@@ -15,19 +15,7 @@ class LoginService
     public function handleLogin(LoginRequest $request): mixed
 {
     if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
-        $role = auth()->user()->roles->pluck('name')[0];
-        switch ($role) {
-            case 'alumni':
-                return redirect()->route('alumni.dashboard-alumni')->with('success', 'Berhasil Login.');
-            case 'admin':
-                return redirect()->route('dashboard')->with('success', 'Berhasil Login.');
-            case 'company':
-                return redirect()->route('company-dashboard')->with('success', 'Berhasil Login.');
-            case 'student':
-                return redirect()->route('dashboard-student')->with('success', 'Berhasil Login.');
-            default:
-                return redirect()->back()->withErrors(trans('auth.login_failed'))->withInput();
-        }
+        return redirect()->route('dashboard')->with('success', 'Berhasil Login.');
     } else {
         return redirect()->back()->withErrors(trans('auth.login_failed'))->withInput();
     }
