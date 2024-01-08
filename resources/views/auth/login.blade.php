b7993fa01cf6dfd9f2ad447e3ca0123222a5ea66
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-01-08 08:48:57 +0700

    error validasi login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,6 +44,13 @@
                                 <p class="mb-0 text-dark fs-6">Masukkan Email dan Kata Sandi!!</p>
                             </div>
                             <div class="form-body">
+                                @if ($errors->any())
+                                    <div>
+                                        @foreach ($errors->all() as $error)
+                                            <p class="text-danger">{{ $error }}</p>
+                                        @endforeach
+                                    </div>
+                                @endif
                                 <form action="{{ route('login') }}" method="post" class="row g-3">
                                     @csrf
                                     <div class="col-12">
@@ -60,7 +67,8 @@
                                     <div class="col-12">
                                         <label for="inputChoosePassword" class="form-label">Password</label>
                                         <div class="input-group" id="show_hide_password">
-                                            <input type="password" name="password" class="form-control border-end-0 @error('password') is-invalid @enderror"
+                                            <input type="password" name="password"
+                                                class="form-control border-end-0 @error('password') is-invalid @enderror"
                                                 id="inputChoosePassword" placeholder="Masukkan Password">
                                             <a href="javascript:;" class="input-group-text bg-transparent"><i
                                                     class="bx bx-hide"></i></a>
@@ -100,7 +108,7 @@
                                 <a href="{{ route('register.company') }}">
                                     <img width="100%" class="rounded-3" src="{{ asset('perusahaan.png') }}" alt=""
                                         srcset="">
-                                        <h6 class="text-center mb-5 mt-2">Perusahaan</h6>
+                                    <h6 class="text-center mb-5 mt-2">Perusahaan</h6>
                                 </a>
                             </div>
                             <div class="col d-flex justify-content-start">
