eb6551fe255f7e72eaad29558b7dcdf4a000c702
paksiswanto <unitedkuryu@gmail.com>
2024-01-17 13:59:24 +0700

    Update detail-portofolio.blade.php

--- resources/views/student/detail-portofolio.blade.php
+++ resources/views/student/detail-portofolio.blade.php
@@ -21,28 +21,28 @@
     <div class="card-body">
         <div class="d-flex justify-content-between">
             <div>
-                <h4 class="mb-3">
+                <h3 class="mb-3 fw-semibold">
                     {{ $portofolio->name }}
-                </h4>
+                </h3>
             </div>
-            <div class="me-4">
-                <h4>{{ $portofolio->year }}</h4>
+            <div class="me-4 fw-semibold">
+                <h3>{{ $portofolio->year }}</h3>
             </div>
         </div>
        
-        <h4 class="mb-3 mt-5">
+        <h3 class="mb-3 mt-5 fw-semibold">
             Foto Foto Project
-        </h4>
+        </h3>
         <div class="row">
             @foreach ($portofolio->photoPortofolios as $photo)
-            <div class="col-2 mt-2">
-                <img src="{{ asset('storage/'.$photo->photo) }}" width="200px" alt="">
+            <div class="col-sm-6 col-lg-3 col-md-6 mt-3">
+                <img src="{{ asset('storage/'.$photo->photo) }}" style="width:100%" alt="">
             </div>
             @endforeach
            
         </div>
-        <h4 class="mb-3 mt-5">Deskripsi</h4>
-        <p class="mt-2">
+        <h3 class="mb-3 mt-5 fw-semibold">Deskripsi</h3>
+        <p class="mt-2 fs-4 fw-normal">
            {{$portofolio->description}}
         </p>
        
