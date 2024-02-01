2848ffdd1ec256870a1a4ee1ce16ffa11da5febc
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-02-01 11:17:41 +0700

    update

--- app/Services/Auth/LoginService.php
+++ app/Services/Auth/LoginService.php
@@ -21,14 +21,23 @@ class LoginService
         $user = $user->getWhere($data);
         if ($user->email_verified_at) {
             if ($user->roles[0]->name != RoleEnum::ADMIN->value) {
-                if ($user->student ? $user->student->status == StatusEnum::ACTIVE->value : $user->company->status == StatusEnum::ACTIVE->value) {
+                if ($user->roles[0]->name == RoleEnum::STAFF->value) {
                     if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
                         return redirect()->route('dashboard')->with('success', 'Berhasil Login.');
                     } else {
                         return redirect()->back()->withErrors(trans('auth.login_failed'))->withInput();
                     }
-                } else {
-                    return redirect()->back()->withErrors('Anda tidak dapat login sekarang, tunggu admin mengkonfirmasi akun anda');
+                }
+                else {
+                    if ($user->student ? $user->student->status == StatusEnum::ACTIVE->value : $user->company->status == StatusEnum::ACTIVE->value) {
+                        if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+                            return redirect()->route('dashboard')->with('success', 'Berhasil Login.');
+                        } else {
+                            return redirect()->back()->withErrors(trans('auth.login_failed'))->withInput();
+                        }
+                    } else {
+                        return redirect()->back()->withErrors('Anda tidak dapat login sekarang, tunggu admin mengkonfirmasi akun anda');
+                    }
                 }
             } else {
                 if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
